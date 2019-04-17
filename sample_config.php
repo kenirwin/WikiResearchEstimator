b83b424d15c03132604a52f4185371b6e48bf6e7
Ken Irwin <kirwin@wittenberg.edu>
2019-04-17 16:19:28 -0400

    adde sample_config, added real config to gitignore

---
+++ sample_config.php
@@ -0,0 +1,7 @@
+<?php
+/* copy this file to config.php and fill in the constant values */
+
+define (EDS_USER, '');
+define (EDS_PASSWORD, '');
+define (EDS_GATEWAY, ''); // this is written with a widgets.ebscohost.com address in mind
+?>
