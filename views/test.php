7d8284a2b973ed985f19ea3d1db403cc13573102
Ken Irwin <kirwin@wittenberg.edu>
2019-04-23 21:19:58 -0400

    added test view

--- views/test.php
+++ views/test.php
@@ -1,13 +1,35 @@
 <?php
-$first = get_declared_classes();
-
 require_once '../vendor/autoload.php';
 require_once '../config.php';
 
 use WikiResearch\Estimator;
-$last = get_declared_classes();
-
-//var_dump(array_diff($first, $last));
-print_r($first);
-print_r($last);
 ?>
+<form>
+<input type="text" name="qs"><input type="submit">
+</form>
+<?php        
+     if (array_key_exists('qs',$_REQUEST)) {
+         try {
+             $eds = new Estimator();
+             $str = $_REQUEST['qs'];
+             $eds->query($str);
+
+             $underscores = preg_replace('/ +/','_',strtolower(chop($str)));
+             $uri = EDS_GATEWAY . $eds->queryString;
+             print '<span class="output">'.PHP_EOL;
+             print 'curl "'.$uri.'" > demo_results_'.$underscores.'.xml';
+             print '</span>'.PHP_EOL;
+             /*
+               $eds->fetch($uri);
+               $eds->loadXmlObject();
+               $eds->getFacets();
+               
+               print_r($eds->facets)
+             */
+         } catch (Exception $e) {
+             print '<pre>';
+             var_dump($e);
+         }
+     }
+     
+     ?>
